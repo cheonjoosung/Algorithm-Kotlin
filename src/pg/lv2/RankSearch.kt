a8d296d9ac4550abc8d420d657be7031d0d78e2f
cheonjoosung <cjswntjd@gmail.com>
2021-02-01 17:21:33 +0900

    add ranksearch but not timer efficency

--- src/pg/lv2/RankSearch.kt
+++ src/pg/lv2/RankSearch.kt
@@ -4,35 +4,45 @@ class RankSearch {
     fun solution(info: Array<String>, query: Array<String>): IntArray {
         var answer = IntArray(query.size)
 
-        /*
-        var info = arrayOf("java backend junior pizza 150","python frontend senior chicken 210",
-        "python frontend senior chicken 150", "cpp backend senior pizza 260","java backend junior chicken 80",
-        "python backend senior chicken 50")
-    var query = arrayOf("java and backend and junior and pizza 100","python and frontend and senior and chicken 200",
-        "cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100",
-        "- and - and - and - 150")
-         */
+        var idx = 0
 
-        var cnt = 0
-        for (temp in query) {
-            println("String is $temp")
+        for (que in query) {
+            println(que)
+            val token = que.replace("and ", "").split(" ")
+            var cnt = 0
 
-            val token = temp.split("and")
-
-
-            for (info in info) {
-                val isPossible = false
+            for (inf in info) {
+                var isPossible = true
 
                 for (value in token) {
+                    if (value == "-") continue
 
+                    if (value.toIntOrNull() ?: -1 != -1) {
+                        val temp = inf.split(" ").last()
+
+                        if (temp.toIntOrNull() ?: -1 != -1) {
+                            if (value.toInt() > temp.toInt()) {
+                                isPossible = false
+                                break
+                            }
+                        }
+                    } else {
+                        if (!inf.contains(value)) {
+//                            println("B $inf $value")
+                            isPossible = false
+                            break;
+                        }
+                    }
                 }
-
-                if (isPossible) cnt = cnt+1
+                if (isPossible) cnt += 1
+//                println("[$idx] $inf $isPossible $cnt")
             }
+
+            answer[idx++] = cnt
         }
 
-
-
+//        for (temp in answer)
+//            println(temp)
 
         return answer
     }
