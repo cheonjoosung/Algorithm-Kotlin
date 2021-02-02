5a93f6ee1304667cd9cb7ec4d2ee82b61a6e22db
cheonjoosung <cjswntjd@gmail.com>
2021-02-02 15:43:01 +0900

    whm

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,8 @@
 import pg.lv2.*
 
 fun main () {
-    val formulaMax = FormulaMax()
+    val fmx = FormulaMax()
 
-    val rankSearch = RankSearch()
-
-    var info = arrayOf("java backend junior pizza 150","python frontend senior chicken 210",
-        "python frontend senior chicken 150", "cpp backend senior pizza 260","java backend junior chicken 80",
-        "python backend senior chicken 50")
-
-    var query = arrayOf("java and backend and junior and pizza 100","python and frontend and senior and chicken 200",
-        "cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100",
-        "- and - and - and - 150")
-    //[1,1,1,1,2
-
-    println(rankSearch.solution(info, query))
+    println(fmx.solution("100-200*300-500+20")) //60420
+    println(fmx.solution("50*6-3*2")) //300
 }
--- src/pg/lv2/FormulaMax.kt
+++ src/pg/lv2/FormulaMax.kt
@@ -2,13 +2,38 @@ package pg.lv2
 
 class FormulaMax {
     fun solution (expression: String): Long {
+        var expPrior = arrayOf("*+-", "*-+", "-*+", "-+*", "+*-", "+-*")
+
         var answer: Long = 0
         /*
         1. * + - 에 총 몇개 있는지 파악하여 배열 만들기
         2. * + - 에 대한 우선순위를 완전탐색하는 경우의 수 생성
         3. 경우의 수에 따라 각 수식 계산
+        "100-200*300-500+20" 60420
+        "50*6-3*2" 300
+        절대값이 가장 큰 값
          */
 
+        var tokenNum = expression.split(Regex("[-+*]"))
+        var tokenExp = expression.split(Regex("[0-9]+"))
+        tokenExp = tokenExp.subList(1, tokenExp.size-1)
+
+        for (expArr in expPrior.iterator()) {
+            
+        }
+
         return answer
+    }
+
+    fun cal(n1: Long, n2: Long, exp: Char) : Long {
+        var res: Long = 0
+
+        when (exp) {
+            '*' -> res = n1*n2
+            '-' -> res = n1-n2
+            '+' -> res = n1+n2
+        }
+
+        return res
     }
 }
