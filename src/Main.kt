8a5e2bafac480d6c9df23f234f062cf9572e68b6
cheonjoosung <cjswntjd@gmail.com>
2021-02-09 16:23:53 +0900

    class

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,8 @@
 import pg.lv2.*
 
 fun main () {
-    val td = TwoDigit()
-
-    println(td.solution("110010101001")) //60420
-    println(td.solution("01110")) //300
-    println(td.solution("1111111")) //300
+    var num = 15
+    var n2 = num.toString(2)
+    var str = n2.toInt(2)
+    println("$n2 $str")
 }
