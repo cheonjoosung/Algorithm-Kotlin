0cb97f02c3c60a8da5a916b8c1c3f13b4c573c34
천주성 <cjswntjd@cheonjuseong-ui-iMac.local>
2021-03-15 22:44:26 +0900

    solve pg lv2 filenamesort

--- src/pg/lv2/FileNameSort.java
+++ src/pg/lv2/FileNameSort.java
@@ -1,8 +1,6 @@
 package pg.lv2;
 
-import com.sun.corba.se.spi.presentation.rmi.IDLNameTranslator;
-
-import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,7 +27,7 @@ public class FileNameSort {
     }
 
     public static String[] solution(String[] files) {
-        String[] answer = {};
+        String[] answer = new String[files.length];
 
         /*
         head(문자구성 한글자이상)/number(1에서5사이의연속숫자)/tail(그 이외에 모두 공백이 될수도있음) 로 나누기
@@ -37,7 +35,7 @@ public class FileNameSort {
         2. 정렬하기
          */
 
-
+        ArrayList<S> list = new ArrayList<>();
         //"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"}
         for (int i = 0; i < files.length; i++) {
             String temp = files[i];
@@ -56,10 +54,33 @@ public class FileNameSort {
                 String number = temp.substring(numStart, numEnd + 1);
                 String tail = temp.substring(numEnd + 1);
 
-                System.out.println(head + " " + number + " " + tail);
+//                System.out.println(head + " " + number + " " + tail);
+                list.add(new S(head, number, tail, i));
             }
+        }
 
+        list.sort((o1, o2) -> {
+            if (o1.head.toLowerCase().equals(o2.head.toLowerCase())) {
+                int n1 = Integer.parseInt(o1.number);
+                int n2 = Integer.parseInt(o2.number);
 
+                return n1-n2;
+                /*if (n1 < n2) {
+                    return 1;
+                } else if (n1 == n2) {
+                    //원래 순서대로
+                    return 0;
+                } else {
+                    return -1;
+                }*/
+            } else {
+                return o1.head.toLowerCase().compareTo(o2.head.toLowerCase());
+            }
+        });
+
+        for (int i = 0 ; i<list.size() ; i++) {
+            S s = list.get(i);
+            answer[i] = s.head+s.number+s.tail;
         }
 
         return answer;
@@ -90,10 +111,12 @@ class S {
     String head;
     String number;
     String tail;
+    int idx;
 
-    S(String head, String number, String tail) {
+    S(String head, String number, String tail, int idx) {
         this.head = head;
         this.number = number;
         this.tail = tail;
+        this.idx = idx;
     }
 }
