c959742fe5de062a171ebb7f70c27b306a3c04c6
천주성 <cjswntjd@gmail.com>
2021-04-25 11:51:56 +0900

    add pg lv2 candidate key

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,22 @@
 import backjun.s1.Bachu
 import etc.Exchange
 import etc.Train
+import pg.lv2.CandidateKey
 import pg.lv2.MenuRenewal
 import java.util.*
 
 fun main() {
 
-    val ex = Train()
-    ex.main()
+    val ex = CandidateKey()
+    var arr = arrayOf(
+        arrayOf("100", "ryan", "music", "2"),
+        arrayOf("200", "apeach", "math", "2"),
+        arrayOf("300", "tube", "computer", "3"),
+        arrayOf("400", "con", "computer", "4"),
+        arrayOf("500", "muzi", "music", "3"),
+        arrayOf("600", "apeach", "music", "2")
+    )
+    println(ex.solution(arr))
 }
 
 
--- src/pg/lv2/CandidateKey.kt
+++ src/pg/lv2/CandidateKey.kt
@@ -1,26 +1,86 @@
 package pg.lv2
 
-class CandidateKey {
-//    [["100","ryan","music","2"],["200","apeach","math","2"],
-//    ["300","tube","computer","3"],["400","con","computer","4"],
-//    ["500","muzi","music","3"],["600","apeach","music","2"]]
+import java.util.ArrayList
+import java.util.HashMap
 
-   //유일성을 만족하는 최소의 후보키가 몇개인가? 위에서는 2개 [이름, 전공]
+//https://velog.io/@hyeon930/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%ED%9B%84%EB%B3%B4%ED%82%A4-Java
+class CandidateKey {
+//    [["100","ryan","music","2"]
+//    ["200","apeach","math","2"],
+//    ["300","tube","computer","3"],
+//    ["400","con","computer","4"],
+//    ["500","muzi","music","3"],
+//    ["600","apeach","music","2"]]
+
+    //유일성을 만족하는 최소의 후보키가 몇개인가? 위에서는 2개 [이름, 전공]
     fun solution(relation: Array<Array<String>>): Int {
-        var answer = 0
+        val candidateKey = ArrayList<Int>()
+
+        val row = relation.size
+        val col = relation[0].size
 
         /*
-        1. 선택한 ( 학번, 이름, 전공, 학년 ) 키의 중복이 있는지 체크
-        2. 키 이외의 나머지 값들에서 중복이 발생하는지 체크
-        3-1. 예를들면 학번을 선택하면 100,200,300,400,500,600 에서 중복이 안됨
-        3-2. 나머지 벨류에서 이름 중복 (appeach), 전공 중복 (컴퓨터,뮤직), 학년 중복(2, 3)
-        4. 정렬 활용하여 앞뒤로 체크?
+            1. 모든 속성의 조합 구한다
+            2. 만들어진 조합이 이전에 만들어진 후보키 포함 여부
+                2-1. 포함하면 다음조합으로
+                2-2. 포함하지 않으면 유일성 체크하여 후보키 여부 판단
+            3. 후보키 갯수 반환
          */
+        for (set in 1 until (1 shl col)) { //모든 조합구하기
+            println("set : ${set.toString(2)}")
 
-       for (i in 1..4) {
+            // 2-1 최소성 검사 : (1) 을 이전에 포함했다면 그다음 조합에서 (1,2)가 되더라도 최소성 만족 못함
+            if (!isMinimal(set, candidateKey)) continue
 
-       }
+            // 2-2 유일성 검사
+            if (isUnique(set, row, col, relation)) {
+                println("Unique : ${set.toString(2)}")
+                candidateKey.add(set)
+            }
+        }
 
-        return answer
+        return candidateKey.size
+    }
+
+    private fun isUnique(
+        set: Int,
+        row: Int,
+        col: Int,
+        relation: Array<Array<String>>
+    ): Boolean {
+        val map = HashMap<String, String>()
+
+        // 자릿수 구하기
+        for (i in 0 until row) {
+            var dataByKeySet = ""
+
+            for (th in 0 until col) {
+                // 1 << th th번째 자릿수 1 << 3 = 1000(2)
+                // set & (1 << 소) 해당 set과 같은 자리 찾아서 키 만드는 작업
+                    println("$set ${1 shl th} ${set and (1 shl th) != 0}")
+                if (set and (1 shl th) != 0) {
+                    // set 10 2번째 인덱스와 같은 것만 키로 만드는 작업
+                    dataByKeySet += relation[i][th]
+                }
+            }
+
+            println("dataByKetSet : $dataByKeySet")
+
+            if (map.containsKey(dataByKeySet))
+                return false
+            else
+                map[dataByKeySet] = dataByKeySet
+        }
+
+        return true
+    }
+
+    private fun isMinimal(set: Int, candidateKey: ArrayList<Int>): Boolean {
+        for (key in candidateKey) {
+            // and 교집합으로 key가 나옴
+            if (key and set == key) return false
+        }
+
+        return true
     }
 }
