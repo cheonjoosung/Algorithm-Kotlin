24c1a9d16a94bb5bebef5556be7aadf05849d274
cheonjoosung <cjswntjd@gmail.com>
2021-07-13 23:47:05 +0900

    프로그래머스 lv3 베스트앨범

--- src/Main.kt
+++ src/Main.kt
@@ -2,12 +2,15 @@ import backjun.s1.SetPresentation
 import backjun.s1.Tomato
 import pg.lv1.CountDividor
 import pg.lv2.ArrayRotation
+import pg.lv3.BestAlbum
 import pg.lv3.StarSequence
 import pg.lv3.TripRoute
 
 fun main() {
 
-    val s = SetPresentation()
-    s.main()
+    val s = BestAlbum()
+    val sArr = arrayOf("classic", "pop", "classic", "classic", "pop")
+    val intArr = intArrayOf(500, 600, 150, 800, 2500)
+    println(s.solution(sArr, intArr))
 }
 
---
+++ src/pg/lv3/BestAlbum.kt
@@ -0,0 +1,14 @@
+package pg.lv3
+
+class BestAlbum {
+
+    fun solution(genres: Array<String>, plays: IntArray): IntArray {
+
+        return genres.indices.groupBy { genres[it] } // { classic = [0,2,3], pop = [1,4] }
+            .toList() // Pair<K, V> [(classic, [0,2,3]) , (pop, [1,4])]
+            .sortedByDescending { it -> it.second.sumBy { plays[it] } } // [(pop, [1,4]), (classic, [0,2,3])]
+            .map { it -> it.second.sortedByDescending { plays[it] }.take(2) } // [[4,1],[3,0]]
+            .flatten() // [4, 1, 3, 0]
+            .toIntArray()
+    }
+}
