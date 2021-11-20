e6c033adeb2db64204484b442d7dff06a48e7eb2
cheonjoosung <cjswntjd@gmail.com>
2021-11-20 13:01:52 +0900

    cache

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,12 @@
-import backjun.s1.SetPresentation
-import backjun.s1.Tomato
-import pg.lv1.CountDividor
-import pg.lv2.ArrayRotation
-import pg.lv3.BestAlbum
+import pg.lv1.MinimumRectangular
 import pg.lv3.EditPyo
-import pg.lv3.StarSequence
-import pg.lv3.TripRoute
 
 fun main() {
 
-    val s = EditPyo()
+    val a1 = arrayOf(intArrayOf(60, 50), intArrayOf(30, 70), intArrayOf(60, 30), intArrayOf(80, 40))
 
-    val cmd1 = arrayOf("D 2","C","U 3","C","D 4","C","U 2","Z","Z")
-    val cmd2 = arrayOf("D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C")
-
-    println(s.solution(8, 2, cmd1))
-    println(s.solution(8, 2, cmd2))
+    val s = MinimumRectangular()
+    println(s.solution(a1))
 }
+
 
