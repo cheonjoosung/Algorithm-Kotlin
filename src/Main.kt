c2cb1edbd8f74308f9a3e0381acf7d7ecedfcfc4
cheonjoosung <cjswntjd@gmail.com>
2021-11-21 22:39:48 +0900

    프로그래머스 lv2 설정자르기 구현 (메모리 문제 해결 필요) & 설정 파일 수정

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,14 @@
 import pg.lv1.MinimumRectangular
+import pg.lv2.CutNArray
 import pg.lv3.EditPyo
 
 fun main() {
 
     val a1 = arrayOf(intArrayOf(60, 50), intArrayOf(30, 70), intArrayOf(60, 30), intArrayOf(80, 40))
 
-    val s = MinimumRectangular()
-    println(s.solution(a1))
+    val s = CutNArray()
+    println(s.solution(3, 2, 5))
+    println(s.solution(4, 7, 14))
 }
 
 
--- src/pg/lv2/CutNArray.kt
+++ src/pg/lv2/CutNArray.kt
@@ -1,33 +1,47 @@
 package pg.lv2
 
-import java.util.ArrayList
-import java.util.HashMap
+import java.util.*
 
-class CandidateKey {
+class CutNArray {
 
     fun solution(n: Int, left: Long, right: Long): IntArray {
-        var answer: IntArray = intArrayOf()
 
-        var arr = Array(n) { IntArray(n) { 0 } }
-        var visited = Array(n) { BooleanArray(n) { false } }
+        val arr = Array(n) { IntArray(n) { 0 } }
+        val d = arrayOf(intArrayOf(0, 1), intArrayOf(1, 0), intArrayOf(1, 1))
 
-        visited[0][0] = true
         arr[0][0] = 1
 
-        var q = java.util.Queue<Pos>
+        val q = LinkedList<Pos>()
         q.add(Pos(0, 0, 1))
 
-        while (q.isEmpty()) {
+        while (!q.isEmpty()) {
 
+            val p = q.poll()
+
+            for (i in 0..2) {
+                val nx = p.x + d[i][0]
+                val ny = p.y + d[i][1]
+
+                if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue
+                if (arr[nx][ny] != 0) continue
+
+                arr[nx][ny] = p.value + 1
+                q.add(Pos(nx, ny, p.value + 1))
+            }
         }
 
-        return answer
+        for (i in arr.indices) {
+            for (j in arr[i].indices) {
+                print("${arr[i][j]} ")
+            }
+            println()
+        }
+
+        val list = mutableListOf<Int>()
+        for (temp in arr) list.addAll(temp.toMutableList())
+
+        return list.subList(left.toInt(), right.toInt()+1).toIntArray()
     }
 
-    private fun bfs(n: Int, array: Array<IntArray>, visited: Array<BooleanArray>) {
-
-
-    }
-
-    data class Pos(val x: Int, val y: Int, value: Int)
+    data class Pos(val x: Int, val y: Int, val value: Int)
 }
