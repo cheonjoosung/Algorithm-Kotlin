42a1bccfb99a7326c89bf12ff572c5103b3a777d
cheonjoosung <cjswntjd@gmail.com>
2021-11-22 20:00:16 +0900

    프로그래머스 lv2 설정자르기 구현

--- src/pg/lv2/CutNArray.kt
+++ src/pg/lv2/CutNArray.kt
@@ -5,43 +5,13 @@ import java.util.*
 class CutNArray {
 
     fun solution(n: Int, left: Long, right: Long): IntArray {
+        val arr = arrayListOf<Int>()
 
-        val arr = Array(n) { IntArray(n) { 0 } }
-        val d = arrayOf(intArrayOf(0, 1), intArrayOf(1, 0), intArrayOf(1, 1))
-
-        arr[0][0] = 1
-
-        val q = LinkedList<Pos>()
-        q.add(Pos(0, 0, 1))
-
-        while (!q.isEmpty()) {
-
-            val p = q.poll()
-
-            for (i in 0..2) {
-                val nx = p.x + d[i][0]
-                val ny = p.y + d[i][1]
-
-                if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue
-                if (arr[nx][ny] != 0) continue
-
-                arr[nx][ny] = p.value + 1
-                q.add(Pos(nx, ny, p.value + 1))
-            }
+        for (i in left..right) {
+            val num = Math.max(i / n, i % n) + 1
+            arr.add(num.toInt())
         }
 
-        for (i in arr.indices) {
-            for (j in arr[i].indices) {
-                print("${arr[i][j]} ")
-            }
-            println()
-        }
-
-        val list = mutableListOf<Int>()
-        for (temp in arr) list.addAll(temp.toMutableList())
-
-        return list.subList(left.toInt(), right.toInt()+1).toIntArray()
+        return arr.toIntArray()
     }
-
-    data class Pos(val x: Int, val y: Int, val value: Int)
 }
