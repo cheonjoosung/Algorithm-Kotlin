98464fba957d878c1d310e8039f24228e254b065
cheonjoosung <cjswntjd@gmail.com>
2022-03-05 16:41:01 +0900

    백준 g5 숨바꼭질

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,15 @@
-import pg.lv1.MinimumRectangular
-import pg.lv2.CutNArray
-import pg.lv3.EditPyo
+import pg.lv3.TrackConstruction
 
 fun main() {
 
-    val a1 = arrayOf(intArrayOf(60, 50), intArrayOf(30, 70), intArrayOf(60, 30), intArrayOf(80, 40))
+    val str: String? = null
 
-    val s = CutNArray()
-    println(s.solution(3, 2, 5))
-    println(s.solution(4, 7, 14))
+    str?.let {
+        println("notNull")
+    } ?: run {
+        println("null")
+    }
+
+    println("hi")
 }
-
 
