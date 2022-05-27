f9a71b50f834aea02801ef0dc9906c473acd7cdb
cheonjoosung <cjswntjd@gmail.com>
2022-05-27 18:56:11 +0900

    머지 에 따른 수정내용

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import backjun.s1.P11286;
+import backjun.s1.P1629;
+import backjun.s2.P4948;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        new P4948().main();
+    }
+}
+
+
