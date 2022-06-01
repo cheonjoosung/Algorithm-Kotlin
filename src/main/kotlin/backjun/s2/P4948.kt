57ce6a75fa042ee8f8d3f6f90605aaf17542abbf
cheonjoosung <cjswntjd@gmail.com>
2022-06-01 13:29:55 +0900

    .

--- src/main/kotlin/backjun/s2/P4948.kt
+++ src/main/kotlin/backjun/s2/P4948.kt
@@ -3,43 +3,40 @@ package backjun.s2
 import java.lang.StringBuilder
 import java.util.*
 
-class P4948 {
+fun main() {
+    val sc = Scanner(System.`in`)
 
-    fun main() {
-        val sc = Scanner(System.`in`)
+    val max = 123_456 * 2
+    val isNotPrime = BooleanArray(max + 1) { false }
 
-        val max = 123_456 * 2
-        val isNotPrime = BooleanArray(max + 1) { false }
+    isNotPrime[0] = true
+    isNotPrime[1] = true
 
-        isNotPrime[0] = true
-        isNotPrime[1] = true
+    val sb = StringBuilder()
 
-        val sb = StringBuilder()
+    for (i in 2..max) {
+        if (isNotPrime[i]) continue
 
-        for (i in 2..max) {
-            if (isNotPrime[i]) continue
-
-            for (j in 2 until max) {
-                if (j * i > max) break
-                isNotPrime[i * j] = true
-            }
+        for (j in 2 until max) {
+            if (j * i > max) break
+            isNotPrime[i * j] = true
         }
-
-        while (true) {
-            val num = sc.nextInt()
-            if (num == 0) break
-
-            var cnt = 0
-            for (i in num + 1..2 * num) {
-                if (i > max) continue
-                if (!isNotPrime[i]) cnt++
-            }
-
-            sb.append(cnt).append("\n")
-        }
-
-        println(sb.toString())
-
-        sc.close()
     }
+
+    while (true) {
+        val num = sc.nextInt()
+        if (num == 0) break
+
+        var cnt = 0
+        for (i in num + 1..2 * num) {
+            if (i > max) continue
+            if (!isNotPrime[i]) cnt++
+        }
+
+        sb.append(cnt).append("\n")
+    }
+
+    println(sb.toString())
+
+    sc.close()
 }
