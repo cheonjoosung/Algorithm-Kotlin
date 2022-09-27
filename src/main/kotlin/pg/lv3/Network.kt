c3da7df3bbe72fe5f577c59386db2964c2c5fb6a
cheonjoosung <cjswntjd@gmail.com>
2022-09-27 19:45:39 +0900

    프로그래머스 lv3 네트워크

--- src/main/kotlin/pg/lv3/Network.kt
+++ src/main/kotlin/pg/lv3/Network.kt
@@ -1,26 +1,43 @@
 package pg.lv3
 
+fun main() {
+
+    val a1 = arrayOf(intArrayOf(1, 1, 0), intArrayOf(1, 1, 0), intArrayOf(0, 0, 1))
+    val a2 = arrayOf(intArrayOf(1, 1, 0), intArrayOf(1, 1, 1), intArrayOf(0, 1, 1))
+
+    val network = Network()
+
+    println(network.solution(3, a1))
+    println(network.solution(3, a2))
+}
+
 class Network {
+
+    private val disconnected = mutableListOf<Int>()
+
     fun solution(n: Int, computers: Array<IntArray>): Int {
+
         var answer = 0
 
-        for (arr in computers) {
+        for (i in 0 until n) disconnected.add(i)
 
-            var first = -1
-            var second = -1
-
-            for (i in arr.indices) {
-                if (arr[i] != 0) {
-                    if (first != -1) first = i
-                    else second = i
-                }
-
-                if (second != -1) break
-            }
-
-
+        while (disconnected.size > 0) {
+            answer++
+            dfs(n, computers, disconnected.removeAt(0))
         }
 
         return answer
+    }
+
+    private fun dfs(n: Int, computers: Array<IntArray>, computer: Int) {
+
+        for (i in 0 until n) {
+            // 두 컴퓨터가 연결되어 있는 경우
+            if (disconnected.contains(i) && computers[computer][i] == 1) {
+                // 네트워크에 속한 컴퓨터 제거
+                disconnected.remove(i)
+                dfs(n, computers, i)
+            }
+        }
     }
 }
