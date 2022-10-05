693bebae83da42d37096fab7325dcdb0d3dd5f7e
cheonjoosung <cjswntjd@gmail.com>
2022-10-05 19:17:03 +0900

    프로그래머스 lv0 두수의곱

---
+++ src/main/kotlin/pg/lv0/TwoNumMul.kt
@@ -0,0 +1,10 @@
+package pg.lv0
+
+class TwoNumMul {
+
+    fun solution(num1: Int, num2: Int): Int {
+        return mul(num1, num2)
+    }
+
+    private fun mul(num1: Int, num2: Int) = num1 * num2
+}
