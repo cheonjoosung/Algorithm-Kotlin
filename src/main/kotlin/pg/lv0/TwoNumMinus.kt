ef4b217616db61e0b57eaf14cb0e873c410131e2
cheonjoosung <cjswntjd@gmail.com>
2022-10-05 19:16:57 +0900

    프로그래머스 lv0 두수의차이

---
+++ src/main/kotlin/pg/lv0/TwoNumMinus.kt
@@ -0,0 +1,10 @@
+package pg.lv0
+
+class TwoNumMinus {
+
+    fun solution(num1: Int, num2: Int): Int {
+        return minus(num1, num2)
+    }
+
+    private fun minus(num1: Int, num2: Int) = num1 - num2
+}
