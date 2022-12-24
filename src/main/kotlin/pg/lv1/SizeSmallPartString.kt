e7d114284aed91135094373b82be0057d6d9a1b6
cheonjoosung <cjswntjd@gmail.com>
2022-12-24 10:43:59 +0900

    프로그래머스 lv1 작은 크기의 부분 문자열 (람다식 추가)

--- src/main/kotlin/pg/lv1/SizeSmallPartString.kt
+++ src/main/kotlin/pg/lv1/SizeSmallPartString.kt
@@ -22,4 +22,10 @@ class SizeSmallPartString {
         }
         return answer
     }
+
+    fun solution2(t: String, p: String): Int {
+        return (0..t.length - p.length)
+            .map { t.substring(it until it + p.length) }
+            .count { it <= p }
+    }
 }
