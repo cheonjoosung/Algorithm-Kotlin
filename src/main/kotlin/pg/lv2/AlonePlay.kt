e94b857d62635627d5285ed4218133a57042599f
cheonjoosung <cjswntjd@gmail.com>
2023-06-27 23:01:09 +0900

    m,,,

--- src/main/kotlin/pg/lv2/AlonePlay.kt
+++ src/main/kotlin/pg/lv2/AlonePlay.kt
@@ -1,34 +1,35 @@
 package pg.lv2
 
 class AlonePlay {
+    var answer: Int = 0
     fun solution(cards: IntArray): Int {
-        val boxes = mutableMapOf<Int, Int>()
-        cards.forEachIndexed { index, card ->
-            boxes[index + 1] = card
-        }
 
-        val groups = mutableListOf<Int>()
-        while (boxes.isNotEmpty()) {
-            val visited = mutableSetOf<Int>()
+        val maxSize = cards.size - 1
+        /**
+         * maxSize 가 되는 모든 경우의 수를 구하는...
+         * 1,6 1,5,1 1,4,2 1,3,3 , 1,2,4
+         */
 
-            var pos = boxes.keys.first()
-            while (pos !in visited) {
-                visited.add(pos)
-                val temp = boxes[pos]
-                boxes.remove(pos)
-                if (temp != null) {
-                    pos = temp
-                }
-            }
-            groups.add(visited.size)
+        var arr = intArrayOf()
+        for (n in 2 until maxSize) {
+            dfs(1, 0, n, maxSize, 0, 1)
         }
-        groups.sortDescending()
-        return if (groups.size > 1) groups[0] * groups[1] else 0
+        return answer
     }
 
+    private fun dfs(idx: Int, cnt: Int, n: Int, maxSize: Int, sum: Int, mul: Int) {
+        if (cnt == n && sum == maxSize) {
+            answer = answer.coerceAtLeast(mul)
+            return
+        }
 
+        for (i in idx until maxSize) {
+            if (sum + i > maxSize) continue
+            dfs(i, cnt+1, n, maxSize, sum+i, mul*i)
+        }
+    }
 }
 
 fun main() {
-    AlonePlay().solution(intArrayOf(8, 6, 3, 7, 2, 5, 1, 4)).also { println(it) }
+    AlonePlay().solution(intArrayOf(8,6,3,7,2,5,1,4)).also { println(it) }
 }
